f3bfe8c190533286953f22c527c7fc9aa83d6c23
Nitish Kumar <mintu.nitish@gmail.com>
2017-10-31 14:48:04 +0530

    Integrated Vuex

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,9 +1,9 @@
-
 /**
  * First we will load all of this project's JavaScript dependencies which
  * include Vue and Vue Resource. This gives a great starting point for
  * building robust, powerful web applications using Vue and Laravel.
  */
+
 
 require('./bootstrap');
 
@@ -16,6 +16,9 @@ require('./bootstrap');
 Vue.component('friend', require('./components/Friend.vue'));
 Vue.component('notification', require('./components/Notification.vue'));
 
+import { store } from "./store"
+
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    store
 });
