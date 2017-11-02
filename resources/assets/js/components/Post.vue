2a60ab55c702f4a5b39ec9a37ce0140ca7bafc5a
Nitish Kumar <mintu.nitish@gmail.com>
2017-11-02 21:56:00 +0530

    Added Post Model

---
+++ resources/assets/js/components/Post.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <textarea class="form-control" rows="5"></textarea>
+                        <button class="btn btn-success pull-right">
+                            Create Post
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+
+        }
+    }
+</script>
--- resources/assets/js/components/UnreadNots.vue
+++ resources/assets/js/components/UnreadNots.vue
@@ -8,7 +8,7 @@
 </template>
 
 <script>
-    export default () {
+    export default {
         mounted() {
             this.get_unread()
         },
