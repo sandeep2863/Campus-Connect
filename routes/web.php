966116eaec59b1b1cecc794071f0780faa63888f
Nitish Kumar <mintu.nitish@gmail.com>
2017-11-14 13:49:36 +0530

    Routes Added

--- routes/web.php
+++ routes/web.php
@@ -55,4 +55,12 @@ Route::group(['middleware' => 'auth'], function () {
     Route::get('/like/{id}', 'LikesController@like');
 
     Route::get('/unlike/{id}', 'LikesController@unlike');
+
+    Route::group(['prefix' => 'messages'], function () {
+        Route::get('/', ['as' => 'messages', 'uses' => 'MessagesController@index']);
+        Route::get('create', ['as' => 'messages.create', 'uses' => 'MessagesController@create']);
+        Route::post('/', ['as' => 'messages.store', 'uses' => 'MessagesController@store']);
+        Route::get('{id}', ['as' => 'messages.show', 'uses' => 'MessagesController@show']);
+        Route::put('{id}', ['as' => 'messages.update', 'uses' => 'MessagesController@update']);
+    });
 });
