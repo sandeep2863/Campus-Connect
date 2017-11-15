93a7b8b956e2d12814878dd39ab7bafb60446cdc
Nitish Kumar <mintu.nitish@gmail.com>
2017-11-15 21:46:41 +0530

    Added Laravel-Messenger
    Signed-off-by: Nitish Kumar <mintu.nitish@gmail.com>

--- config/app.php
+++ config/app.php
@@ -167,7 +167,7 @@ return [
          * Package Service Providers...
          */
 
-        //
+        Cmgmyr\Messenger\MessengerServiceProvider::class,
 
         /*
          * Application Service Providers...
